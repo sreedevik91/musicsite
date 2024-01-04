a85e42f62cc0f0543b8ac99c374694975f07a17f
sreedevik91 <155008187+sreedevik91@users.noreply.github.com>
2024-01-04 18:04:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function like(value){
+    if(value=='1'){
+        document.getElementById("smiley").style.visibility="visible";
+        document.getElementById("smiley").src="https://s3-whjr-v2-prod-bucket.whjr.online/0d314d9a-7b1c-4c76-985d-72a336e6521e.png";
+    }else{
+        document.getElementById("smiley").style.visibility="visible";
+        document.getElementById("smiley").src="https://s3-whjr-v2-prod-bucket.whjr.online/7d0f842f-0ba8-41a8-b5d9-3584d1a416ab.png";
+    }
+}
